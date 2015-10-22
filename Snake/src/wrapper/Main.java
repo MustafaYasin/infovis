5d3b1a62398f162a5e22a4f39a32ab7fc1d147e6
Julian Lorenz <Julianlorenz@dhcp-10-176-88-243.dynamic.eduroam.mwn.de>
2015-10-22 18:11:34 +0200

    main erbt von app

--- Snake/src/wrapper/Main.java
+++ Snake/src/wrapper/Main.java
@@ -1,5 +1,19 @@
 package wrapper;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	
 }
