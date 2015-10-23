72b5768185e7b828476c22e74689c83213d2aaac
Yasna Mindilikova <yasna.mindilikova@gmail.com>
2015-10-23 16:09:15 +0300

    SnakeInterface that holds constant values needed in the game added to the Model

---
+++ Snake/src/wrapper/model/SnakeInterface.java
@@ -0,0 +1,21 @@
+package wrapper.model;
+
+/**
+ * Created by Yasna on 10/23/2015.
+ */
+
+/**interface that has game-values that never change like speed
+ * radius of the circle usw
+ * @author Yasna
+ */
+public interface SnakeInterface {
+	
+	    double snakeSpeed = 2;
+	    double radius = 8;
+
+	    int GAME_WIDTH = 800;
+
+	    int GAME_HEIGHT = GAME_WIDTH * 9/16;
+
+}
+
